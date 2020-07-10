5e33539682790d20106d94a310a0ed84462be6ff
IlhamShiddiq <shdqillham123@gmail.com>
2020-07-10 16:12:45 +0700

    nav minor fixing (change save icon)

--- sw.js
+++ sw.js
@@ -3,6 +3,21 @@ var urlsToCache = [
   "/",
   "/nav.html",
   "/index.html",
+  "/detail-team.html",
+  "/manifest.json",
+  "/pages/favorit.html",
+  "/pages/ligue-1.html",
+  "/pages/primera-division.html",
+  "/pages/seriea.html",
+  "/src/image/nobar.png",
+  "/src/script/api.js",
+  "/src/script/font-awesome.js",
+  "/src/script/materialize.min.js",
+  "/src/script/nav.js",
+  "/src/style/font-awesome.css",
+  "/src/style/materialize.min.css",
+  "/src/style/responsive.css",
+  "/src/style/style.css",
 ];
  
 self.addEventListener("install", function(event) {
