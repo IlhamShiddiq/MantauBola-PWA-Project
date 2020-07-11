4707421599f502823e38cf30a24cd302b0d63b40
IlhamShiddiq <shdqillham123@gmail.com>
2020-07-11 12:12:25 +0700

    offline access fixing

--- src/script/api.js
+++ src/script/api.js
@@ -1,4 +1,4 @@
-var base_url = "https://api.football-data.org/";
+var base_url = "https://api.football-data.org/v2/";
 
 function status(response) {
   if (response.status !== 200) {
@@ -21,7 +21,19 @@ function error(error) {
 // Ligue 1
 function getData(id) {
 
-    fetch(base_url + `v2/competitions/${id}/`, {
+    if ('caches' in window) {
+      caches.match(base_url + `competitions/${id}/`).then(function(response) {
+        if (response) {
+          response.json()
+          .then(function (data) {
+            isiKonten(data);
+            getStandings(id);
+          })
+        }
+      })
+    }
+
+    fetch(base_url + `competitions/${id}/`, {
             headers: {
                 "X-Auth-Token": "e9cc4588ffe7402d86183b403094e7d6"
             },
@@ -31,14 +43,57 @@ function getData(id) {
         .then(function(data) {
         console.log(data);
             isiKonten(data);
-            getStandings(id)
+            getStandings(id);
         })
-        .catch(error);
-}
+        .catch(error);  
+  }
 
 function getStandings(id) {
 
-    fetch(base_url + `v2/competitions/${id}/standings/`, {
+    if ('caches' in window) {
+      caches.match(base_url + `competitions/${id}/standings/`).then(function(response) {
+        if (response) {
+          response.json().then(function (data) {
+            // Showing Total
+          const t_st = document.getElementById("stTotal");
+          t_st.innerHTML = `<h6 class="stand-total">${data.standings[0].table.length} Total</h6>`;
+            var listStand = "";
+            console.log(data.standings[0].table);
+            data.standings[0].table.forEach(function(data) {
+              listStand += `
+                    <a href="#" class="col xl8 offset-xl2 l8 offset-l2 m10 offset-m1 s12 sch-detail hoverable">
+                      <div class="card-sch left-align">
+                            <img src="${data.team.crestUrl}" height="70" width="80" alt="${data.team.name}">
+                          <div class="name-stand white-color">
+                              ${data.position}. ${data.team.name}
+                              <div class="points center-align">
+                                  ${data.points} Points
+                              </div>
+                          </div>
+                          
+                          <div class="info right-align">   
+                              <div class="result win d-inline center-align">
+                                  ${data.won} Won
+                              </div>
+                              <div class="result lose d-inline center-align">
+                                  ${data.lost} Lost
+                              </div>
+                              <div class="result draw d-inline center-align">
+                                  ${data.draw} Draw
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+                  `;
+            });
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("st").innerHTML = "Standings";
+            document.getElementById("standings").innerHTML = listStand;
+          })
+        }
+      })
+    }
+    fetch(base_url + `competitions/${id}/standings/`, {
             headers: {
                 "X-Auth-Token": "e9cc4588ffe7402d86183b403094e7d6"
             },
--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = "firstpwa-v1";
+const CACHE_NAME = "mantaubola-v2";
 var urlsToCache = [
   "/",
   "/nav.html",
   "/index.html",
   "/detail-team.html",
   "/manifest.json",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png",
   "/pages/favorit.html",
   "/pages/home.html",
   "/pages/ligue-1.html",
   "/pages/primera-division.html",
   "/pages/seriea.html",
+  "/pages/champions-l.html",
   "/src/image/nobar.png",
   "/src/script/api.js",
   "/src/script/font-awesome.js",
   "/src/script/materialize.min.js",
   "/src/script/nav.js",
+  "/src/script/jquery.js",
+  "/src/script/reg-sw.js",
   "/src/script/swiper-opt.js",
   "/src/script/swiper.js",
   "/src/style/font-awesome.css",
@@ -31,6 +42,26 @@ self.addEventListener("install", function(event) {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("fetch", function(event) {
+  var base_url = "https://api.football-data.org/v2/";
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        })
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+          return response || fetch (event.request);
+      })
+    )
+  }
 });
 
 self.addEventListener("activate", function(event) {
